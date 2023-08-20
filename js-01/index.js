1144a7944dfab3b85982abb3fa5231775fd1bf77
monesKeshavarz7878@gmail.com <mones.keshavarz7878@gmail.com>
2023-08-20 22:25:12 +0430

    feat/monesKeshavarz/Doing the first JavaScript exercise

---
+++ js-01/index.js
@@ -0,0 +1,4 @@
+const number1 = + prompt("Enter a number1");
+const number2 = + prompt("Enter a number2");
+console.log(`${number1} + ${number2} = ${number1 + number2}`);
+document.write(`${number1} + ${number2} = ${number1 + number2}`);
