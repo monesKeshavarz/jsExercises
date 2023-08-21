3af4448f03e5f55bdd8dd0daf108634e3161feb4
monesKeshavarz7878@gmail.com <mones.keshavarz7878@gmail.com>
2023-08-21 17:25:24 +0430

    feat/monesKeshavarz/Doing the fourth JavaScript exercise which consists of three parts

---
+++ js-04/js-04-03/index.js
@@ -0,0 +1,12 @@
+const mobile = {
+  Color: "white",
+  Model: "A30s",
+  brand: "samsung",
+
+  add: function () {
+console.log("Added to cart");
+  },
+  remove : function () {
+    console.log("Removed from cart");
+      }
+};
