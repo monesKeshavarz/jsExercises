f63a4279a0585d3aa3c1cca40639dba487ae37bb
monesKeshavarz7878@gmail.com <mones.keshavarz7878@gmail.com>
2023-08-22 16:56:49 +0430

    bug/monesKeshavarz/Fix the masking problem in the array

--- js-05/index.js
+++ js-05/index.js
@@ -4,19 +4,22 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-const fillList = () =>{
+const processing = () =>{
     const randomNumber = Math.floor(Math.random()*10);
     const listWord = ["mones","ali", "hossin", "keshavarz","maasomi"]
     const randomWord =  listWord[Math.floor(Math.random()*listWord.length)];
     const randomDate = Date.now()+ getRandomInt(1000, 4000);
-
-    newlist = {
-        randomNumber,
-        randomDate,
-        randomWord,
+    return {
+      randomNumber,
+      randomDate,
+      randomWord,
     }
-
-    list.push(newlist);
 }
-fillList();
+
+const pushItem = () =>{
+  list.push(processing());
+}
+
+pushItem();
+pushItem();
 console.log(list);
